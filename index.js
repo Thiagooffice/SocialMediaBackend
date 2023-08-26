a889518cb1119049b96c050c2172081b7363a8a1
Thiago <thiagooffice2@gmail.com>
2023-08-25 23:13:38 -0300

    feat: add database mongodb with mongoose

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ import { fileURLToPath } from "url";
 import morgan from "morgan";
 
 /*Configurations*/
-const __filename = fileURLToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginEmbedderPolicy({ policy: "cross-origin" }))
-app.use(morgan("common"))
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /*File Storage*/
 const storage = multer.diskStorage({
@@ -33,3 +33,15 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage });
+
+/*Mongoose setup*/
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
