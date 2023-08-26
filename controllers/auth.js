c4b7f5e39224dc1fe13a1f9bc2c7aead7c593669
Thiago <thiagooffice2@gmail.com>
2023-08-25 23:32:04 -0300

    feat: creating controller and models page and User model

---
+++ controllers/auth.js
@@ -0,0 +1,8 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+/* Register user*/
+export const register = async (req, res) => {
+    
+}
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import path from "path";
 import { fileURLToPath } from "url";
 import morgan from "morgan";
+import {register} from "./controllers/auth.js"
 
 /*Configurations*/
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +34,9 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES*/
+app.post("/auth/register", upload.single("picture"), register)
 
 /*Mongoose setup*/
 const PORT = process.env.PORT || 6001;
