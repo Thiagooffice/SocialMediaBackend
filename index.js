a6387c095846712a0a6a1f1b34805d580dd88831
Thiago <thiagooffice2@gmail.com>
2023-08-29 19:12:22 -0300

    feat: post routes setup

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
+import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import morgan from "morgan";
-import authRoutes from "./routes/auth.js"
-import userRoutes from "./routes/users.js"
-import postRoutes from "./routes/post.js"
-import {register} from "./controllers/auth.js"
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /*Configurations*/
 const __filename = fileURLToPath(import.meta.url);
@@ -40,6 +42,7 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES*/
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /*ROUTES*/
 app.use("/auth", authRoutes);
